712aecbf936fac1dfd52c305140a5895de0448f6
Samuel Ang <samuel.ang.prog@gmail.com>
2023-07-04 12:20:34 -0500

    Added proc gen examples

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float damage = 0;
+    
+    public void Launch(Quaternion rotation, float speed, float newDamage){
+        damage = newDamage;
+        transform.rotation = rotation;
+        GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+        Destroy(this.gameObject,10);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Wall"){
+            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            Destroy(this.gameObject,3f);
+        }
+    }
+}
